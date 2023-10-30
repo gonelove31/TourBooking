dd03486c971485fa65a5c82178a2a6ac39aa3203
cuong03pl <cuongculebarca@gmail.com>
2023-10-30 14:54:49 +0700

    fix conflict 2

--- Models/TourContext.cs
+++ Models/TourContext.cs
@@ -14,8 +14,10 @@ namespace BookingTour.Models
             base.OnConfiguring(optionsBuilder);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
+
         {
             base.OnModelCreating(modelBuilder);
+
             foreach (var entityType in modelBuilder.Model.GetEntityTypes())
             {
                 var tableName = entityType.GetTableName();
@@ -24,11 +26,12 @@ namespace BookingTour.Models
                     entityType.SetTableName(tableName.Substring(6));
                 }
             }
+           
         }
 
         public DbSet<Booking>? bookings { set; get; }
         public DbSet<Location> locations { set; get; }
         public DbSet<Tours> tours { set; get; }
-
+       
     }
 }
