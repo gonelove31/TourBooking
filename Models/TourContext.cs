a5215361ab7caeada476a0e84a6e711156208417
cuong03pl <cuongculebarca@gmail.com>
2023-10-30 14:51:08 +0700

    fix conflict 1

--- Models/TourContext.cs
+++ Models/TourContext.cs
@@ -16,6 +16,14 @@ namespace BookingTour.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
         }
 
         public DbSet<Booking>? bookings { set; get; }
