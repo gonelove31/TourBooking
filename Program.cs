2e7e681c6c6cb94639d1d53514602e09ad957a3b
cuong03pl <cuongculebarca@gmail.com>
2023-10-30 14:44:41 +0700

    test

--- Program.cs
+++ Program.cs
@@ -1,29 +1,91 @@
 using BookingTour.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using static Album.Mail.SendMailService;
+using  Album.Mail;
+using System.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using App.Menu;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using App.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var services = builder.Services;
 
-services.AddRazorPages();
+var services = builder.Services;
 services.AddDbContext<TourContext>(options =>
 {
     string url = builder.Configuration.GetConnectionString("TourContext");
     options.UseSqlServer(url);
 });
 
+services.AddRazorPages();
+
+services.AddIdentity<AppUser, IdentityRole>()
+      .AddEntityFrameworkStores<TourContext>()
+      .AddDefaultTokenProviders();
+
+//Identity/Account/Login
+// dang ky indentity , role : vai tro
+
+
+services.Configure<IdentityOptions>(options => {
+    // Thiết lập về Password
+    options.Password.RequireDigit = false; // Không bắt phải có số
+    options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+    options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+    options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+    options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+    options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+    // Cấu hình Lockout - khóa user
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Cấu hình về User.
+    options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+    // Cấu hình đăng nhập.
+    options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+    options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+    options.SignIn.RequireConfirmedAccount = true; // cau hinh bat xac thuc email moi duoc dang ky
+});
 
 
 
-services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<TourContext>().AddDefaultTokenProviders();
 
-var app = builder.Build();
+
+services.AddOptions();
+var mailsetting =builder.Configuration.GetSection("MailSettings");
+services.Configure<MailSettings>(mailsetting);
+services.AddSingleton<IEmailSender, SendMailService>();
+
+
+
+
+services.AddTransient<IActionContextAccessor, ActionContextAccessor>();
+services.AddTransient<AdminSidebarService>();
+services.AddAuthorization(options => {
+    options.AddPolicy("ViewManageMenu", builder => {
+        builder.RequireAuthenticatedUser();
+        builder.RequireRole(RoleName.Administrator);
+    });
+});
+
 
 // Configure the HTTP request pipeline.
+var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -33,16 +95,17 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.MapControllers();
 app.UseRouting();
 
+app.UseAuthentication(); //xac thuc danh tinh
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthorization(); // sau khi xac thuc , thi dc lam gi
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+//doc thong tin 1 user , kiem tra 1 user co dang nhap khong 
 
-
+app.MapRazorPages();
 app.Run();
